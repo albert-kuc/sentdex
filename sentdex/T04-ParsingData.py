64c73547dd8b206d31b6163f6ca9215ab963ce26
Albert <albert.kuc@gmail.com>
2018-04-17 22:22:14 +0100

    Open files and read specified value

--- sentdex/T04-ParsingData.py
+++ sentdex/T04-ParsingData.py
@@ -1,7 +1,9 @@
 """
     Parsing data:
-    1. Locate all files in specified directory.
-    2. Pulling data from file in specified location.
+    4.1. Locate all files in specified directory.
+    4.2. Pulling datetime and unix time from file in specified location.
+    5.1. Read every file in each directory
+    5.2. Strip file data to specified value with source.split
 
     https://pythonprogramming.net/getting-data-machine-learning/
 """
@@ -21,13 +23,28 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
 
     for each_dir in stock_list[1:]:                     # [1:] first stock list is initial pattern
         each_file = os.listdir(each_dir)
-        if len(each_file) > 0:
+        ticker = each_dir.split('\\')[1]                # ticker is the stock folder name containing files
+        if len(each_file) > 0:                          # ignore empty files
             for file in each_file:
 
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')   # save file name as date value
 
                 unix_time = time.mktime(date_stamp.timetuple())
                 print(date_stamp, unix_time)
+                full_file_path = each_dir+'/'+file      # creates path to all files
+                print(full_file_path)
+                source = open(full_file_path, 'r').read()   # use urllib urlopen for url link
+                # print(source)
+
+                """
+                    Source split: 
+                    First define the part of code used for split. 
+                    [1] means to leave everything from first following element on the right.
+                    Second split element with [0] defines to remove everything including element defined for split.
+                """
+                value = source.split(gather+':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
+                print(value)
+                print(ticker+':', value)
                 time.sleep(5)
 
 
